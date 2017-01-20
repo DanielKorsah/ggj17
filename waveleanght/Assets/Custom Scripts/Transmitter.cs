430cb588a8520af1a596c3a069cc9d28b23ac97f
Dan's Laptop <dkorsah15@aol.co.uk>
2017-01-20 19:34:07 +0000

    Change Wavelength on click

---
+++ waveleanght/Assets/Custom Scripts/Transmitter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transmitter : MonoBehaviour {
+    [SerializeField]
+    List<GameObject> Walls;
+
+    List<string> state = new List<string>(new string[] { "visable", "infrared", "ultraviolet" });
+    string wavelength;
+    int i = 0;
+
+    // Use this for initialization
+    void Start () {
+        wavelength = state[i];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnMouseDown()
+    {
+        int i = 0;
+        wavelength = state[i + 1];
+        Debug.Log("Wavelength is " + wavelength);
+    }
+}
