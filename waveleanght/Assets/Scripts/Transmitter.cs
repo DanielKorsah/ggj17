6d1c2a54636750e8dce26f1d2aababa22a401c05
Paulius Bieksa <priverstinis@gmail.com>
2017-01-21 16:30:04 +0000

    bug fixed.

--- waveleanght/Assets/Scripts/Transmitter.cs
+++ waveleanght/Assets/Scripts/Transmitter.cs
@@ -93,7 +93,7 @@ public class Transmitter : MonoBehaviour
             {
                 //inv.AddBoostPickup();
             }
-            else
+            else if (inv.FocusPickup > 0)
             {
 
                 inv.SubFocusPickup();
