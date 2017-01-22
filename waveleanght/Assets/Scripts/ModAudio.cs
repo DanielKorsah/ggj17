9748bc3aa4b91f89925ef63e107ddff958679c82
Dan's Laptop <dkorsah15@aol.co.uk>
2017-01-22 14:04:37 +0000

    Apply (some) audio
    Former-commit-id: 50d6fe9689ee3a9a4691aef19d8f5c46f41d416d

---
+++ waveleanght/Assets/Scripts/ModAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModAudio : MonoBehaviour {
+
+    [SerializeField]
+    Sprite defaultIcon;
+
+    Sprite currentIcon;
+    bool alreadyPlayed;
+
+	// Use this for initialization
+	void Start () {
+        currentIcon = GetComponent<Sprite>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(currentIcon != defaultIcon && !alreadyPlayed)
+        {
+            
+        }
+		
+	}
+}
