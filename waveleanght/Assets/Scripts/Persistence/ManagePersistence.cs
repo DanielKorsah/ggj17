b8a2f74908a8b3b0cfb8a0599b81c18ca4d369cd
Daniel Korsah <dkorsah15@aol.co.uk>
2017-05-05 00:06:43 +0100

    Best time saving for levels.
    Former-commit-id: 308e4fa3246994894cd7e0acfca5f10b99c8ac2e

---
+++ waveleanght/Assets/Scripts/Persistence/ManagePersistence.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+
+public class ManagePersistence : MonoBehaviour {
+
+    void onDestroy()
+    {
+        if (gameObject.name == "EndPortal")
+        {
+            HighScore();
+        }
+    }
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    void HighScore()
+    {
+        float timer = GameObject.Find("Timer").GetComponent<TimeLimit>().time;
+
+        string scene = SceneManager.GetActiveScene().name;
+        string ct_write;
+        string hs_path = Application.dataPath + scene + "_HighScore.Json";
+
+        //get the time from this run
+        CompletionTime ct = new CompletionTime();
+        ct.t = Math.Round(timer, 3);
+
+        //serialise to Json
+        ct_write = JsonUtility.ToJson(ct);
+
+        //if no data exists post time
+        if (!File.Exists(hs_path))
+        {
+            File.WriteAllText(hs_path, ct_write);
+        }
+        else
+        {
+            //read current best time data
+            CompletionTime ct_Read = new CompletionTime();
+            ct_Read = JsonUtility.FromJson<CompletionTime>(hs_path);
+
+            //if time is faster overwrite with new time
+            if(ct.t < ct_Read.t)
+            {
+                File.WriteAllText(hs_path, ct_write);
+            }
+        }
+    }
+}
+
+[Serializable]
+public class CompletionTime
+{
+    public double t;
+}
