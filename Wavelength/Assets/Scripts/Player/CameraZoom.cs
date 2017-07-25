7d18375fd38312051b643fac73e63626897f3c5d
Daniel Korsah <dkorsah15@aol.co.uk>
2017-07-25 23:23:49 +0100

    Fix saving issue.

---
+++ Wavelength/Assets/Scripts/Player/CameraZoom.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+
+    bool toggle = false;
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        if(Input.GetKeyDown("c"))
+        {
+            toggle = !toggle;
+        }
+
+        if (toggle)
+        {
+            Camera.main.orthographicSize = 12;
+        }
+        else
+        {
+            Camera.main.orthographicSize = 8;
+        }
+	}
+}
