d28bc6470ef18df6c2e12da09d4b1ae2d3869ebb
Tim-Surface <40219124@live.napier.ac.uk>
2017-08-30 17:43:14 +0100

    Added a unit test project to the solution.

---
+++ UnitTestWavelength/BitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestWavelength
+{
+    [TestClass]
+    public class BitTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
