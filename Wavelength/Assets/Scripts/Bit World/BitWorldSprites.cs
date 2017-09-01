a47c6fbfd70cf3e2caf482fbbbe86278f436ab6e
TIM-DESKTOP\Tim <t.s.sanderson@hotmail.co.uk>
2017-09-01 21:15:15 +0100

    BitWorldSprites:   - Singleton instantiation method   - Added functions for adding to dictionaries   - Removed monobehaviour inheritance

--- Wavelength/Assets/Scripts/Bit World/BitWorldSprites.cs
+++ Wavelength/Assets/Scripts/Bit World/BitWorldSprites.cs
@@ -2,22 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BitWorldSprites : MonoBehaviour
+public class BitWorldSprites
 {
+    // Dictionaries of sprites
     Dictionary<BitType, Sprite> bitSprites = new Dictionary<BitType, Sprite>();
     Dictionary<WallShape, Sprite> wallShapeSprites = new Dictionary<WallShape, Sprite>();
 
-    // Use this for initialization
-    void Start()
-    {
+    // Private instance of this class (singleton)
+    static BitWorldSprites instance;
 
+    // Private constructor (singleton)
+    private BitWorldSprites() { }
+
+    // Singleton instatioation method
+    static public BitWorldSprites Instantiate
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new BitWorldSprites();
+            }
+            return instance;
+        }
     }
 
+    // Add sprite to dictionary
+    public void AddBitSprite(BitType bt, Sprite sprite)
+    {
+        bitSprites.Add(bt, sprite);
+    }
+
+    // Add wall shape to dictionary
+    public void AddWallShapeSprite(WallShape ws, Sprite sprite)
+    {
+        wallShapeSprites.Add(ws, sprite);
+    }
+
+    // Get the sprite for a BitType
     public Sprite GetBitSprite(BitType bt)
     {
         return bitSprites[bt];
     }
 
+    // Get the sprite for a WallShape
     public Sprite GetWallShapeSprite(WallShape ws)
     {
         return wallShapeSprites[ws];
