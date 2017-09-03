5628c5ff984b81e3ff2ad5f21f61b0e7d43f83ba
TIM-DESKTOP\Tim <t.s.sanderson@hotmail.co.uk>
2017-09-03 20:27:52 +0100

    Finished conversion classes.

--- Wavelength/Assets/Scripts/Bit World/BitTypetoSprite.cs
+++ Wavelength/Assets/Scripts/Bit World/BitTypetoSprite.cs
@@ -1,16 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class BitTypetoSprite : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+[System.Serializable]
+public class BitTypetoSprite : MonoBehaviour
+{
+    public BitType bitType;
+    public Sprite sprite;
 }
--- Wavelength/Assets/Scripts/Bit World/WavelengthtoSprite.cs
+++ Wavelength/Assets/Scripts/Bit World/WavelengthtoSprite.cs
@@ -1,16 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class WavelengthtoSprite : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+[System.Serializable]
+public class WavelengthtoSprite
+{
+    public Wavelength wavelength;
+    public Sprite sprite;
 }
