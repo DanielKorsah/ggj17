18af635833ca1d3ad7a3bd39de5d2957af11cc30
Dan's Laptop <dkorsah15@aol.co.uk>
2017-09-20 17:01:39 +0100

    Started implementing solutions for scaling.

---
+++ Wavelength/Assets/AudioControls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioControls : MonoBehaviour
+{
+
+
+
+    AudioSource musicLoop;
+
+    void Start()
+    {
+        AudioSource[] sources;
+        sources = GetComponents<AudioSource>();
+
+        musicLoop = sources[0];
+
+    }
+
+    public void MusicMute()
+    {
+        musicLoop.mute = !musicLoop.mute;
+    }
+}
