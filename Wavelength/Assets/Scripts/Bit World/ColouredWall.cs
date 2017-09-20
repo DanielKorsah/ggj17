9711bf5026904097d6b1027981149c3cf5fb76a3
Tim-Surface <40219124@live.napier.ac.uk>
2017-09-20 16:59:42 +0100

    Started work on the coloured wall prefab/script.

---
+++ Wavelength/Assets/Scripts/Bit World/ColouredWall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColouredWall : Bit {
+
+
+	void Awake () {
+        bitType = BitType.Wall;
+        neighbourDependant = true;
+        showColour = false;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        world = FindObjectOfType<World>();
+        spriteSheet = BitWorldSprites.Instantiate;
+        GetNeighbours();
+        GetBitShapeString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
